89f800df1d5c3aefa444a151c67c3a852ce388e6
makl <kolyn9gg@gmail.com>
2023-10-04 12:57:40 +0300

    import класса ship в главный модуль

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -14,7 +15,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_hight))
         pygame.display.set_caption("Alien Invasion")
-        
+        self.ship = Ship(self)
+
     
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -26,6 +28,7 @@ class AlienInvasion:
 
             # При каждом проходе цикла перерисовывается экран.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
 
