16d40ebd3ad6ba96c76490e1ea44ed2d0d3ede89
makl <kolyn9gg@gmail.com>
2023-10-04 13:22:01 +0300

    Рефакторинг кода alien invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,18 +21,23 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+     
+    
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
-
-
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+            
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
     ai = AlienInvasion()
