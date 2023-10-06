07b69c6e99979c680686f9895d309360d5fb234e
makl <kolyn9gg@gmail.com>
2023-10-06 09:31:39 +0300

    Добавленение ограничений перемещений, нельзя выйти за экран

--- ship.py
+++ ship.py
@@ -1,41 +1,41 @@
 import pygame
 
+
 class Ship:
     """Класс для управления кораблем"""
+
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию"""
-        self.screen = ai_game.screen
-        self.settings = ai_game.settings 
-        self.screen_rect  = ai_game.screen.get_rect()
-        
 
-        # Загружает изображение корабля и получает прямоугольник.
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Загружает изображение корабля и получает прямоугольник
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
-        
-        # Сохранение вещественной координаты центра коробля 
+
+        # Сохранение вещественной координаты центра коробя 
         self.x = float(self.rect.x)
-        
+
         # Флаги перемещения
         self.moving_right = False
         self.moving_left = False
-        
-        
+
     def update(self):
-        """Обновление позии корабля с учетом флага"""
+        """Обновление позиции корабля с учетом флага"""
+
         # Обновляется атрибут x, не rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-            
-        #Обновление атрибута rect на основе self.x.
+
+        # Обновление атрибута rect на основе self.x.
         self.rect.x = self.x
-        
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
         self.screen.blit(self.image, self.rect)
-
