d5b34b18649478c31d9a66fcbaab513b76853ed9
makl <kolyn9gg@gmail.com>
2023-10-11 00:55:31 +0300

    Добавление кода коллизии, чтобы прицельцы и снаряд исчезали

--- alien_invasion.py
+++ alien_invasion.py
@@ -68,6 +68,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Проверка попаданий в пришельцев.
+        # При обнаружении попадания удалить снаряд и пришельца.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
         if len(self.bullets) < self.settings.bullets_allowed:
