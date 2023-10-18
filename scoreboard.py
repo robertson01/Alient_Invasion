ccc5c27c12514a33b10a8669ca7c22f983716c58
makl <kolyn9gg@gmail.com>
2023-10-19 01:06:00 +0300

    Новый метод который сохраняет лучший рекорд

--- scoreboard.py
+++ scoreboard.py
@@ -13,6 +13,7 @@ class Scoreboard:
         self.font = pygame.font.SysFont(None, 48)
         # Подготовка исходного изображения
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Преобразует текущий счет в графическое изображение."""
@@ -25,7 +26,25 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(high_score_str, True,
+                                                 self.text_color, self.settings.bg_color)
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def check_high_score(self):
+        """Проверяет, появился ли новый рекорд."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
     def show_score(self):
         """Выводит счет на экран."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
 
