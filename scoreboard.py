440b1f3b76ad96ec742d65e1f07461ab1f49e097
makl <kolyn9gg@gmail.com>
2023-10-19 01:28:36 +0300

    добавление метода и переменой level

--- scoreboard.py
+++ scoreboard.py
@@ -14,6 +14,7 @@ class Scoreboard:
         # Подготовка исходного изображения
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Преобразует текущий счет в графическое изображение."""
@@ -43,8 +44,18 @@ class Scoreboard:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
 
+    def prep_level(self):
+        """Преобразует уровень в графическое изображение."""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True,
+                                            self.text_color, self.settings.bg_color)
+        # Уровень выводится под текущим счетом.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
     def show_score(self):
         """Выводит счет на экран."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
-
+        self.screen.blit(self.level_image, self.level_rect)
