c36bd156ebeb7f35eeb5e1c60cfb1e9b30dc0252
makl <kolyn9gg@gmail.com>
2023-10-19 02:38:31 +0300

    Чтение и запись в файл лучшего результата(рекорда(

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,4 @@
+file_record = 'record.txt'
 class GameStats:
     """Отслеживание статистики  для игры Alien Invasion"""
 
@@ -8,11 +9,22 @@ class GameStats:
         # Игра Alien Invasion запускается в неактивном состоянии.
         self.game_active = False
         # Рекорд не должен сбрасываться.
-        self.high_score = 0
+        self.high_score = self.read_high_score()
+
+    def write_high_score(self):
+        """Запись рекорда в файл после выхода игры"""
+        with open(file_record, 'w') as file:
+            file.write(str(self.high_score))
+
+    def read_high_score(self):
+        with open(file_record, 'r') as file:
+            self.high_score = file.read()
+        return int(self.high_score)
 
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
+
 
